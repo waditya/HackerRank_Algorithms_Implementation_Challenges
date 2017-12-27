8b3efe9524c56f4ef1223450ab175cf11fa8a62d
Aditya Dhananjay Wagholikar <waditya02@gmail.com>
2017-12-27 11:36:32 -0600

    Update 03_Kangaroo.py

--- 03_Kangaroo.py
+++ 03_Kangaroo.py
@@ -4,6 +4,7 @@ import sys
 
 def kangaroo(x1, v1, x2, v2):
     message = None
+    flag = 'NO'
     if(x1 < x2):
         if(v2 >= v1):
             message = 'NO'
@@ -19,7 +20,14 @@ def kangaroo(x1, v1, x2, v2):
             message = 'YES'
         else:
             message = 'NO'
-    return(message)
+    if(message == 'YES'):
+        
+        for i in range(0, 10000 ,1):
+            if x1 + v1*i == x2 + v2*i:
+                flag = 'YES'
+                break;
+            
+    return(flag)
 x1, v1, x2, v2 = input().strip().split(' ')
 x1, v1, x2, v2 = [int(x1), int(v1), int(x2), int(v2)]
 result = kangaroo(x1, v1, x2, v2)
