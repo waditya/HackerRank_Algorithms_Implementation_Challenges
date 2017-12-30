f31738dea14f4694423843a977ef07c094696190
Aditya Dhananjay Wagholikar <waditya02@gmail.com>
2017-12-30 02:20:06 -0600

    Create 08_Migratory_Birds.py

---
+++ 08_Migratory_Birds.py
@@ -0,0 +1,24 @@
+#!/bin/python3
+
+import sys
+
+def migratoryBirds(n, ar):
+    type = 0
+    ar.sort()
+    previous_streak = 1
+    current_streak = 1
+    
+    type_of_maximum = 1
+    
+    for i in range(1, n, 1):
+        if ar[i] == ar[i-1]:
+            current_streak = current_streak + 1
+            if current_streak > previous_streak:
+                previous_streak = current_streak
+                type_of_maximum = ar[i]
+    return(type_of_maximum)
+
+n = int(input().strip())
+ar = list(map(int, input().strip().split(' ')))
+result = migratoryBirds(n, ar)
+print(result)
