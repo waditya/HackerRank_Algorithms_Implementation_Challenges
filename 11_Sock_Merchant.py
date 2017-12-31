760ba0c982dea97b64b2ccc63be416609fbb1bda
Aditya Dhananjay Wagholikar <waditya02@gmail.com>
2017-12-31 13:19:22 -0600

    Update 11_Sock_Merchant.py

--- 11_Sock_Merchant.py
+++ 11_Sock_Merchant.py
@@ -5,15 +5,22 @@ import sys
 def sockMerchant(n, ar):
     ##Sort the array 
     ar.sort()
-    
-    ##Declare and initialize variables
+    br = ar
+    br = list(set(br))
+    ##print(br)
     no_of_pairs = 0
-    
-    for i in range(1, len(ar), 2):
-        if ar[i] == ar[i - 1]:
-            no_of_pairs = no_of_pairs + 1
-        else:
-            i = i - 1
+    counter = 0
+    ##Declare and initialize variables
+    for i in range(0, len(br), 1):
+        for j in range(0, len(ar), 1):
+            ##print(ar[j], br[i])
+            if int(ar[j]/br[i] == 1):
+                ##print("Match found")
+                counter = counter + 1
+        ##print(counter)
+        no_of_pairs = no_of_pairs + int(counter/2)
+        ##print(no_of_pairs)
+        counter = 0
     return(no_of_pairs)
 
 n = int(input().strip())
