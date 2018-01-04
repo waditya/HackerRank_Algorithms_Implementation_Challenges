ec1f3786d59f487264aa0d660946da069b44fbd4
Aditya Dhananjay Wagholikar <waditya02@gmail.com>
2018-01-03 23:05:48 -0600

    Create 20_Designer_PDF_Viewer.py

---
+++ 20_Designer_PDF_Viewer.py
@@ -0,0 +1,29 @@
+#!/bin/python3
+
+import sys
+
+def designerPdfViewer(h, word):
+    #Calculate the ASCII value of character in the word. For [a - z], the ASCII value is [97 - 122]. Subtract 97 from the obtained         ##value and substitute it as the index of array h stating height of each character alphabetically.
+    
+    ##Calculate the width of the word
+    
+    width_of_the_word = len(word)
+    width_of_a_character = 1
+    ##Calculate the maximum possible height of the word
+    
+    maximum_height_of_word = 1
+    
+    for index in range(0, width_of_the_word, 1):
+        if h[ord(word[index]) - 97] > maximum_height_of_word:
+            maximum_height_of_word = h[ord(word[index]) - 97]
+            
+    ##Calculate the area of the highlighted word
+    
+    area = width_of_the_word * width_of_a_character * maximum_height_of_word
+    return(area)
+
+if __name__ == "__main__":
+    h = list(map(int, input().strip().split(' ')))
+    word = input().strip()
+    result = designerPdfViewer(h, word)
+    print(result)
