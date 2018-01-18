ba518f4bd5140ab2e4695c26ad83c155d6c08b2c
Aditya Dhananjay Wagholikar <waditya02@gmail.com>
2018-01-17 20:50:39 -0600

    Update 25_Save_the_prisoner.py

--- 25_Save_the_prisoner.py
+++ 25_Save_the_prisoner.py
@@ -3,16 +3,19 @@
 import sys
 
 def saveThePrisoner(n, m, s):
-    if m > n:
-        return(m%n + s - 1)
-    elif m==n:
-        return(m - (s - 1))
+    remainder = m % n
+    if m > n:    
+        poisoned_person_number = (remainder + (s - 1))% n
+        while  poisoned_person_number > n:
+            poisoned_person_number = poisoned_person_number%n 
+    elif m == n:
+        poisoned_person_number = (s - 1) + int((m - s + 1)/m)
     else:
-        diff = n - m
         if s + m > n:
-            return(n - s + 1)
+            poisoned_person_number = (n - s + 1)
         else:
-            return(s + m -1)
+            poisoned_person_number = (s + m -1)
+    return(poisoned_person_number)
 
 t = int(input().strip())
 for a0 in range(t):
@@ -20,4 +23,3 @@ for a0 in range(t):
     n, m, s = [int(n), int(m), int(s)]
     result = saveThePrisoner(n, m, s)
     print(result)
-
