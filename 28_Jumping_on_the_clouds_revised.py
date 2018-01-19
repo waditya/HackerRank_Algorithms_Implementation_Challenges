28a7c30571dc7b7d3c8b049e673129e2adddf430
Aditya Dhananjay Wagholikar <waditya02@gmail.com>
2018-01-19 12:27:20 -0600

    Update 28_Jumping_on_the_clouds_revised.py

--- 28_Jumping_on_the_clouds_revised.py
+++ 28_Jumping_on_the_clouds_revised.py
@@ -1,1 +1,27 @@
+#!/bin/python3
+
+import sys
+
+def jumpingOnClouds(c, k):
+    ##print(c)
+    ##print(k)
+    energy = 100
+    flag = True
+    i = 0
+    while(flag):
+        i = (i + k)%n
+        if c[i] == 1:
+            energy = energy - 3
+        else:
+            energy = energy - 1
+        if i == 0:
+            flag = False
+    return(energy)
+
+if __name__ == "__main__":
+    n, k = input().strip().split(' ')
+    n, k = [int(n), int(k)]
+    c = list(map(int, input().strip().split(' ')))
+    result = jumpingOnClouds(c, k)
+    print(result)
 
