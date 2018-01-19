f41cbd00f7a3452c3bc78e9fe719c2efddba97e1
Aditya Dhananjay Wagholikar <waditya02@gmail.com>
2018-01-19 13:01:39 -0600

    Update 30_Extralong_Factorial.py

--- 30_Extralong_Factorial.py
+++ 30_Extralong_Factorial.py
@@ -1,1 +1,18 @@
+#!/bin/python3
+
+import sys
+
+def extraLongFactorials(n):
+    result = 1
+    while(n - 1):
+        result = result * n
+        n = n - 1
+        #print(n)
+    print(result)
+    
+    
+
+if __name__ == "__main__":
+    n = int(input().strip())
+    extraLongFactorials(n)
 
