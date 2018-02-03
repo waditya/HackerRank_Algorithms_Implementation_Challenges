3191df0376da136786f438ee091190c776443063
Aditya Dhananjay Wagholikar <waditya02@gmail.com>
2018-02-03 16:08:45 -0600

    Update 33_Library_Fine.py

--- 33_Library_Fine.py
+++ 33_Library_Fine.py
@@ -1,1 +1,32 @@
+#!/bin/python3
+
+import sys
+
+def libraryFine(d1, m1, y1, d2, m2, y2):
+    ##print(d1)
+    ##print(m1)
+    ##print(y1)
+    ##print(d2)
+    ##print(m2)
+    ##print(y2)
+    fine = 0
+    
+    if(y1 > y2):
+        fine = 10000
+    else:
+        if m1 > m2:
+            fine = 500 * (m1 - m2)
+        else:
+            if d1 > d2:
+                fine = 15 * (d1 - d2)
+    return(fine)
+    
+
+if __name__ == "__main__":
+    d1, m1, y1 = input().strip().split(' ')
+    d1, m1, y1 = [int(d1), int(m1), int(y1)]
+    d2, m2, y2 = input().strip().split(' ')
+    d2, m2, y2 = [int(d2), int(m2), int(y2)]
+    result = libraryFine(d1, m1, y1, d2, m2, y2)
+    print(result)
 
