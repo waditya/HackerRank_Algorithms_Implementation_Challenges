b430fdc24101efe72b2462f48bf111bb1dfb5b98
Aditya Dhananjay Wagholikar <waditya02@gmail.com>
2018-02-03 17:02:15 -0600

    Update 35_Non_Divisible_Subset.py

--- 35_Non_Divisible_Subset.py
+++ 35_Non_Divisible_Subset.py
@@ -1,1 +1,23 @@
+#!/bin/python3
+
+import sys
+
+def nonDivisibleSubset(k, arr):
+    #print(k)
+    #print(arr)
+    total = 0
+    for i in range(0, len(arr), 1):
+        for j in range(i+1, len(arr), 1):
+            #print("i, j, i + j :"+str(arr[i])+", "+str(arr[j])+", "+str(arr[i]+arr[j]))
+            if (arr[i]+arr[j]) % k != 0:
+                total +=1
+                #print("Total : "+str(total))
+    return(total)
+
+if __name__ == "__main__":
+    n, k = input().strip().split(' ')
+    n, k = [int(n), int(k)]
+    arr = list(map(int, input().strip().split(' ')))
+    result = nonDivisibleSubset(k, arr)
+    print(result)
 
