7ad953b6d3dc5c18f57211207b2c0bedbcfa2970
Aditya Dhananjay Wagholikar <waditya02@gmail.com>
2018-02-05 15:38:29 -0600

    Update 36_Repeated_String.py

--- 36_Repeated_String.py
+++ 36_Repeated_String.py
@@ -1,1 +1,29 @@
+#!/bin/python3
+
+import sys
+
+def repeatedString(s, n):
+    ##print(s)   
+    ##print(n)
+    lengthOfString = len(s)  ##length of string
+    ##print(lengthOfString)
+    position_of_a_in_string = [index for index, value in enumerate(s) if value == 'a']
+    ##print(position_of_a_in_string)
+    noa = len(position_of_a_in_string)  ##Number of 'a' in string
+    quotient = int(n/lengthOfString)    
+    remainder = int(n % lengthOfString)
+    ##print(quotient)
+    ##print(remainder)
+    remainder_string = s[: remainder]
+    position_of_a_in_remainder_string = [index for index, value in enumerate(remainder_string) if value == 'a']
+    noar = len(position_of_a_in_remainder_string) ##Number of 'a' in remainder string
+    return(noa*quotient + noar)
+    
+    
+
+if __name__ == "__main__":
+    s = input().strip()
+    n = int(input().strip())
+    result = repeatedString(s, n)
+    print(result)
 
