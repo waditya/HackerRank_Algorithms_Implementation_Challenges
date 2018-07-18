876caf01d90b919d60817d4e4c68b1f4044593c7
Aditya Dhananjay Wagholikar <waditya02@gmail.com>
2018-07-18 16:54:11 -0500

    Update 43 Taum and Birthday.java

--- 43 Taum and Birthday.java
+++ 43 Taum and Birthday.java
@@ -1,1 +1,52 @@
 /*Taum and Birthday*/
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the taumBday function below.
+    static int taumBday(int b, int w, int bc, int wc, int z) {
+        return(b*bc+w*wc > (bc > wc? (w+b)*wc + z*b : (b+w)*bc + z*w)?(bc > wc? (w+b)*wc + z*b : (b+w)*bc + z*w):b*bc+w*wc);
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int t = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int tItr = 0; tItr < t; tItr++) {
+            String[] bw = scanner.nextLine().split(" ");
+
+            int b = Integer.parseInt(bw[0]);
+
+            int w = Integer.parseInt(bw[1]);
+
+            String[] bcWcz = scanner.nextLine().split(" ");
+
+            int bc = Integer.parseInt(bcWcz[0]);
+
+            int wc = Integer.parseInt(bcWcz[1]);
+
+            int z = Integer.parseInt(bcWcz[2]);
+
+            int result = taumBday(b, w, bc, wc, z);
+
+            bufferedWriter.write(String.valueOf(result));
+            bufferedWriter.newLine();
+        }
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
+
