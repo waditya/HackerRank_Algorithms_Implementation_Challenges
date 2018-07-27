6ed2e5e4671315f2ccaf8765669af3ed322d5035
Aditya Dhananjay Wagholikar <waditya02@gmail.com>
2018-07-27 18:11:04 -0500

    Update 54 Strange Counter.java

--- 54 Strange Counter.java
+++ 54 Strange Counter.java
@@ -1,1 +1,40 @@
 /*Strange Counter*/
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the strangeCounter function below.
+    static long strangeCounter(long t) {
+        int rem = 3;
+        while(t > rem){
+            t = t-rem;
+            rem *= 2;
+            System.out.println(rem-t+1);
+        }
+        return(rem - t +1);
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        long t = scanner.nextLong();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        long result = strangeCounter(t);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
